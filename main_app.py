c5f79e5db96a34ef1221f22178d6823ab6de9522
Gaidinlung Kamei <gaidink@mba14.local>
2017-07-13 09:49:13 +0530

    structurally functional

---
+++ main_app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import enchant
+from itertools import permutations, combinations
+
+
+puzzle_app = Flask(__name__)
+
+
+@puzzle_app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@puzzle_app.route('/results', methods=['GET', 'POST'])
+def results(anagrams=None, k=None, inp=None, found=None):
+
+    en_US = enchant.Dict("en_US")
+
+    anagrams = []
+    inp = str(request.form['word'])
+    k = int(request.form['word_len'])
+
+    com = [''.join(x) for x in combinations(inp, k)]
+
+    for i in com:
+        perm = [''.join(p) for p in permutations(list(i))]
+
+        for i in perm:
+            if en_US.check(i) and (i != inp):
+                anagrams.append(i)
+    found = len(anagrams)
+    return render_template('results.html', anagrams=anagrams, k=k, inp=inp, found=found)
+
+
+if __name__ == '__main__':
+    puzzle_app.run(debug=True)
